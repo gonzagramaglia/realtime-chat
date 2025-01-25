58317a4ac5e72a2116393a0b1edc01ec68af74d1
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2025-01-25 02:07:49 -0300

    frontend: Chat page. LeftSidebar component

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -1,9 +1,36 @@
+import assets from "../../assets/assets";
 import "./LeftSidebar.css";
 
 const LeftSidebar = () => {
   return (
     <>
-      <div>LeftSidebar</div>
+      <div className="leftside">
+        <div className="leftside-top">
+          <div className="leftside-nav">
+            <img src={assets.logo} alt="logo" className="logo" />
+            <div className="menu">
+              <img src={assets.menu_icon} alt="" />
+            </div>
+          </div>
+          <div className="leftside-search">
+            <img src={assets.search_icon} alt="search" />
+            <input type="text" placeholder="Search..." />
+          </div>
+        </div>
+        <div className="leftside-list">
+          {Array(12)
+            .fill("")
+            .map((item, index) => (
+              <div key={index} className="friends">
+                <img src={assets.profile_img} alt="" />
+                <div>
+                  <p>Chris Brown</p>
+                  <span>Hey, how r u?</span>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
     </>
   );
 };
