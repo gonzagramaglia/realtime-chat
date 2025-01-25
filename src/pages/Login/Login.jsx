cc464d57997a76108de9a0e2ad917f47c17bafb6
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2025-01-24 23:53:35 -0300

    frontend: Login page. Sign up form

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,43 @@
 import "./Login.css";
+import assets from "../../assets/assets.js";
 
 const Login = () => {
   return (
     <>
-      <div>Login Page</div>
+      <div className="login">
+        <img src={assets.logo_big} alt="logo" className="logo" />
+        <form className="login-form">
+          <h2>Sign Up</h2>
+          <input
+            type="text"
+            placeholder="Username"
+            required
+            className="form-input"
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            className="form-input"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            required
+            className="form-input"
+          />
+          <button type="submit">Sign Up</button>
+          <div className="login-term">
+            <input type="checkbox" />
+            <p>Agree to the terms of use & privacy policy</p>
+          </div>
+          <div className="login-forgot">
+            <p className="login-toggle">
+              Already have an account? <span>Click here</span>
+            </p>
+          </div>
+        </form>
+      </div>
     </>
   );
 };
