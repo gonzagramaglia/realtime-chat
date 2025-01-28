27f6b4d26830e9f03fb5aa1164e7b8b1c26e6a62
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2025-01-28 13:37:04 -0300

    frontend: Chat page. ChatBox component. Top & Bottom

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -1,9 +1,27 @@
 import "./ChatBox.css";
+import assets from "../../assets/assets";
 
 const ChatBox = () => {
   return (
     <>
-      <div>ChatBox</div>
+      <div className="chat-box">
+        <div className="chat-user">
+          <img src={assets.profile_img} alt="profile image" />
+          <p>
+            Chris Brown{" "}
+            <img className="dot" src={assets.green_dot} alt="green dot" />
+          </p>
+          <img src={assets.help_icon} className="help" alt="help icon" />
+        </div>
+        <div className="chat-input">
+          <input type="text" placeholder="Send a message" />
+          <input type="file" id="image" accept="image/png, image/jpeg" hidden />
+          <label htmlFor="image">
+            <img src={assets.gallery_icon} alt="gallery icon" />
+          </label>
+          <img src={assets.send_button} alt="send button" />
+        </div>
+      </div>
     </>
   );
 };
