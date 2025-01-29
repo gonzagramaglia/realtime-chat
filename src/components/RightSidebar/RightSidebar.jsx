9051719c96ba5b281d972dc9b7f94bd21fad37ed
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2025-01-29 06:25:55 -0300

    frontend: Chat page. RightSidebar component

--- src/components/RightSidebar/RightSidebar.jsx
+++ src/components/RightSidebar/RightSidebar.jsx
@@ -1,9 +1,31 @@
 import "./RightSidebar.css";
+import assets from "../../assets/assets";
 
 const RightSidebar = () => {
   return (
     <>
-      <div>RightSidebar</div>
+      <div className="rightside">
+        <div className="rightside-profile">
+          <img src={assets.profile_img} alt="profile picture" />
+          <h3>
+            Chris Brown <img src={assets.green_dot} className="dot" />
+          </h3>
+          <p>Hey there, I&apos;m Chris</p>
+        </div>
+        <hr />
+        <div className="rightside-media">
+          <p>Media</p>
+          <div>
+            <img src={assets.pic1} alt="" />
+            <img src={assets.pic2} alt="" />
+            <img src={assets.pic3} alt="" />
+            <img src={assets.pic4} alt="" />
+            <img src={assets.pic1} alt="" />
+            <img src={assets.pic2} alt="" />
+          </div>
+        </div>
+        <button>Logout</button>
+      </div>
     </>
   );
 };
